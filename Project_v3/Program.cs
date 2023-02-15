6c68fca9d4c5f8ced08e44e34732f7b5d9e6de51
Kacper Torba <Kacper.Torba@microsoft.wsei.edu.pl>
2023-02-15 19:46:24 +0100

    Next_test

--- Project_v3/Program.cs
+++ Project_v3/Program.cs
@@ -20,7 +20,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+//niew iem w czym jest problem z gitem
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
